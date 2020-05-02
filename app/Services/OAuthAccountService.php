0999a82ea04a47505282ab9f2b203e6a1c536e7f
Kaizzzoku <ka1zzzoku@gmail.com>
2020-05-02 17:28:40 +0300

    Update token

--- app/Services/OAuthAccountService.php
+++ app/Services/OAuthAccountService.php
@@ -29,12 +29,14 @@ class OAuthAccountService
 
         $new_accs = [];
         $new_billings = [];
-        array_walk($new_acc_ids, function ($item) use ($new_billings, $new_accs, $platform) {
-        	$new_accs[] = ['account_id' => $item, 'oauth_provider_id' => $platform, ];
+        array_walk($new_acc_ids, function ($key) use ($new_billings, $new_accs, $platform) {
+        	$new_accs[] = ['account_id' => $key, 'oauth_provider_id' => $platform, ];
+            $new_billings[] = ['oauth_account_id' => $key];
 
         });
 
         OAuthAccount::insert($new_acc_rows);
+        UnregisteredBilling::insert()
 
         $accs = OAuthAccount::whereIn('account_id', $keys)->get();
 	
