263010054d894340b57590fa36c6669bfeae7795
Kaizzzoku <ka1zzzoku@gmail.com>
2020-05-03 19:09:11 +0300

    Create route for add coins to many accounts

--- app/Services/OAuthAccountService.php
+++ app/Services/OAuthAccountService.php
@@ -4,6 +4,9 @@ namespace App\Services;
 
 use App\Models\OAuthAccount;
 use App\Models\User;
+use Carbon\Carbon;
+use App\Models\UnregisteredBilling;
+use Ramsey\Uuid\Uuid;
 
 class OAuthAccountService
 {
@@ -16,27 +19,31 @@ class OAuthAccountService
 
 	public function getOrCreateMany(array $keys, int $platform)
 	{
+        $now = Carbon::now();
         $db_accs = OAuthAccount::whereIn('account_id', $keys)->get();
 
-        $new_acc_ids = array_diff($data['accounts'], $db_accs->pluck('account_id')->toArray()); 	
-
-        $new_acc_rows = array_map(function ($item) use ($platform) {
-            return [
-            	'account_id' => $item,
-            	'oauth_provider_id' => $platform
-			];
-        }, $new_acc_ids);
+        $new_acc_keys = array_diff($keys, $db_accs->pluck('account_id')->toArray()); 	
 
         $new_accs = [];
         $new_billings = [];
-        array_walk($new_acc_ids, function ($key) use ($new_billings, $new_accs, $platform) {
-        	$new_accs[] = ['account_id' => $key, 'oauth_provider_id' => $platform, ];
-            $new_billings[] = ['oauth_account_id' => $key];
+        foreach ($new_acc_keys as $key) {
+            $new_accs[] = [
+                'id' => $uuid = Uuid::uuid4(),
+                'account_id' => $key, 
+                'oauth_provider_id' => $platform, 
+                'created_at' => $now,
+                'updated_at' => $now,
+            ];
+            $new_billings[] = [
+                'id' => Uuid::uuid4(),
+                'oauth_account_id' => $uuid,
+                'created_at' => $now,
+                'updated_at' => $now,
+            ];
+        }
 
-        });
-
-        OAuthAccount::insert($new_acc_rows);
-        UnregisteredBilling::insert()
+        OAuthAccount::insert($new_accs);
+        UnregisteredBilling::insert($new_billings);
 
         $accs = OAuthAccount::whereIn('account_id', $keys)->get();
 	
--- app/Services/TransactionService.php
+++ app/Services/TransactionService.php
@@ -3,13 +3,15 @@
 namespace App\Services;
 
 use App\Models\Transaction;
-
+use Illuminate\Support\Collection;
+use Carbon\Carbon;
+use Ramsey\Uuid\Uuid;
 class TransactionService
 {
 	public const DUST_COIN_TYPE = 0;
 
 	public function createForDustCoins(
-		float $coins_num,
+		$coins_num,
 		string $owner_id, 
 		int $action,
 		bool $is_registered = true
@@ -22,5 +24,30 @@ class TransactionService
 			'is_registered' => $is_registered,
 			'currency_type' => static::DUST_COIN_TYPE,
 		]);
+	}
+
+	public function createManyForDustCoins(
+		$coins_num,
+		Collection $owners,
+		int $action,
+		bool $is_registered = true
+	)
+	{
+		$now = Carbon::now();
+		$transactions = [];
+		foreach ($owners as $owner) {
+			$transactions[] = [
+				'id' => Uuid::uuid4(),
+				'currency_num' => $coins_num,
+				'owner_id' => $owner->getKey(),
+				'action' => $action,
+				'is_registered' => $is_registered,
+				'currency_type' => static::DUST_COIN_TYPE,
+				'created_at' => $now,
+				'updated_at' => $now,
+			];
+		}
+
+		Transaction::insert($transactions);
 	}
 }
