0b86db0e8132fc08b3e842f501d31cd4b1052c49
Kaizzzoku <ka1zzzoku@gmail.com>
2020-05-09 14:52:48 +0300

    Add model, migration and controller for league

---
+++ app/Models/LeagueRow.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class LeagueRow extends Model
+{
+    protected $table = 'league';
+    protected $guarded = [];
+}
---
+++ database/migrations/2020_05_08_163712_create_league_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLeagueTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('league', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('username', 100);
+            $table->smallInteger('week');
+            $table->smallInteger('class');
+            $table->bigInteger('score')->default(0);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('league');
+    }
+}
