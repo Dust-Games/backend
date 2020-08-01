ee3fe911ef4da9dcb5e9a1687ad53505d4004e2a
Vladimir <elcopro@gmail.com>
2020-08-01 09:25:44 +0300

    New duel rules

--- config/duel_rules.php
+++ config/duel_rules.php
@@ -3,8 +3,8 @@
 return [
     [ 'coins' => 1, 'wins' => 1 ],
     [ 'coins' => 10, 'wins' => 10 ],
-    [ 'coins' => 40, 'wins' => 25 ],
-    [ 'coins' => 80, 'wins' => 50 ],
-    [ 'coins' => 150, 'wins' => 100 ],
-    [ 'coins' => 1000, 'wins' => 500 ],
+    [ 'coins' => 200, 'wins' => 100 ],
+    [ 'coins' => 2000, 'wins' => 500 ],
+    [ 'coins' => 10000, 'wins' => 1500 ],
+    [ 'coins' => 100000, 'wins' => 5000 ],
 ];
